7a345bff45bebb3aaa3342efa0e847330e02ef63
m0l0d0i <iwan.korepanow@yandex.ru>
2025-12-12 03:54:13 +0500

    Работы за 4 и часть 5 семестра

---
+++ 2 курс/pdf/C# (Алгоритмизация и программирование)/BankomatApp/BankomatApp/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankomatApp
+{
+    //Класс Банкнота
+    class Banknote
+    {
+        public int Denomination { get; set; } // Номинал купюры
+        public int Count { get; set; }        // Количество
+
+        //Конструктор
+        public Banknote(int denomination, int count)
+        {
+            Denomination = denomination;
+            Count = count;
+        }
+    }
+    //Класс Банкномата
+    class Bankomat
+    {
+        //List всех банкнот
+        private List<Banknote> _banknots = new List<Banknote>();
+
+        // Инициализация банкомата
+        public void Initialize(int count5000, int count2000, int count1000,
+                               int count500, int count200, int count100)
+        {
+            _banknots.Clear();
+            _banknots.Add(new Banknote(5000, count5000));
+            _banknots.Add(new Banknote(2000, count2000));
+            _banknots.Add(new Banknote(1000, count1000));
+            _banknots.Add(new Banknote(500, count500));
+            _banknots.Add(new Banknote(200, count200));
+            _banknots.Add(new Banknote(100, count100));
+        }
+
+        // Метод выдачи денег
+        public Dictionary<int, int> TryWithdraw(int amount)
+        {
+            //Условие на проверку суммы > 0 и сумма кратна 100р
+            if (amount <= 0 || amount % 100 != 0)
+                return null;
+
+
+            //Хранение количества купюр, которые будут выданы
+            var result = new Dictionary<int, int>();
+            foreach (var note in _banknots)
+                result[note.Denomination] = 0;
+
+            //Банкомат пытается выдать как можно больше крупных купюр (начиная с самых больших номиналов)
+            foreach (var note in _banknots)
+            {
+                int maxPossible = Math.Min(note.Count, amount / note.Denomination);
+                result[note.Denomination] = maxPossible;
+                amount -= maxPossible * note.Denomination;
+
+                if (amount == 0)
+                    break;
+            }
+
+            if (amount != 0)
+                return null;
+
+            // Обновляем количество купюр в банкомате
+            foreach (var note in _banknots)
+            {
+                note.Count -= result[note.Denomination];
+            }
+
+            return result;
+        }
+
+        // Получить текущее состояние банкомата
+        public Dictionary<int, int> GetState()
+        {
+            var state = new Dictionary<int, int>();
+            foreach (var b in _banknots)
+                state[b.Denomination] = b.Count;
+            return state;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bankomat bankomat = new Bankomat();
+
+            Console.WriteLine("Введите начальное состояние банкомата:");
+
+            int fiveThousand = ReadInt("Количество купюр номиналом 5000 рублей: ");
+            int twoThousand = ReadInt("Количество купюр номиналом 2000 рублей: ");
+            int oneThousand = ReadInt("Количество купюр номиналом 1000 рублей: ");
+            int fiveHundred = ReadInt("Количество купюр номиналом 500 рублей: ");
+            int twoHundred = ReadInt("Количество купюр номиналом 200 рублей: ");
+            int oneHundred = ReadInt("Количество купюр номиналом 100 рублей: ");
+
+            bankomat.Initialize(fiveThousand, twoThousand, oneThousand, fiveHundred, twoHundred, oneHundred);
+
+            int amount = ReadInt("Введите сумму для выдачи: ");
+
+            var withdrawn = bankomat.TryWithdraw(amount);
+
+            if (withdrawn == null)
+            {
+                Console.WriteLine("Невозможно выдать указанную сумму");
+            }
+            else
+            {
+                Console.WriteLine("\nВыдано:");
+                int total = 0;
+                foreach (var pair in withdrawn)
+                {
+                    int value = pair.Key * pair.Value;
+                    if (pair.Value > 0)
+                        Console.WriteLine($"Количество купюр номиналом {pair.Key} рублей – {pair.Value} шт.");
+                    total += value;
+                }
+                Console.WriteLine($"Всего выдано – {total} рублей.");
+
+                Console.WriteLine("\nОстаток купюр в банкомате:");
+                var state = bankomat.GetState();
+                foreach (var pair in state)
+                {
+                    Console.WriteLine($"Количество купюр номиналом {pair.Key} рублей – {pair.Value} шт.");
+                }
+            }
+        }
+
+        // Утилитный метод для ввода целых чисел
+        static int ReadInt(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int result) && result >= 0)
+                    return result;
+                Console.WriteLine("Ошибка ввода. Введите корректное неотрицательное число.");
+            }
+        }
+    }
+}
---
+++ 2 курс/pdf/C# (Алгоритмизация и программирование)/Image.csproj/MedianFilterTask.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Recognizer
+{
+	internal static class MedianFilterTask
+	{
+        /* 
+		 * Для борьбы с пиксельным шумом, подобным тому, что на изображении,
+		 * обычно применяют медианный фильтр, в котором цвет каждого пикселя, 
+		 * заменяется на медиану всех цветов в некоторой окрестности пикселя.
+		 * https://en.wikipedia.org/wiki/Median_filter
+		 * 
+		 * Используйте окно размером 3х3 для не граничных пикселей,
+		 * Окно размером 2х2 для угловых и 3х2 или 2х3 для граничных.
+		 */
+        public static double[,] MedianFilter(double[,] original)
+        {
+            var width = original.GetLength(0);
+            var height = original.GetLength(1);
+            var medianPixels = new double[width, height];
+
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                    medianPixels[x, y] = GetMedianPixelValue(x, y, original);
+            return medianPixels;
+        }
+
+        public static double GetMedianPixelValue(int x, int y, double[,] pixels)
+        {
+            var width = pixels.GetLength(0);
+            var height = pixels.GetLength(1);
+            var pixelValues = new List<double>();
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    if (IsPixelInsideBoundaries(x - 1 + i, y - 1 + j, width, height))
+                        pixelValues.Add(pixels[x - 1 + i, y - 1 + j]);
+
+            pixelValues.Sort();
+            if (pixelValues.Count % 2 == 1)
+                return pixelValues[pixelValues.Count / 2];
+            else
+                return (pixelValues[(pixelValues.Count / 2) - 1] + pixelValues[pixelValues.Count / 2]) / 2;
+        }
+
+        public static bool IsPixelInsideBoundaries(int x, int y, int width, int height)
+        {
+            return (x > -1 && y > -1) && (x < width && y < height);
+        }
+    }
+}
